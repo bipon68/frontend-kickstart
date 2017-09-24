e66af58adf465202d1373a9a2e592be7c60aa526
Markus Sommer <markussom@posteo.de>
2017-09-24 07:53:01 +0200

    [TASK] Init gulp-sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('css', function () {
   return gulp.src('./assets/scss/**/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS())
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./css'));
 });
 
