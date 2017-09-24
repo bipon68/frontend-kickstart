62091ada303bbee8ecf67ce92a62856676f91c7a
Markus Sommer <markussom@posteo.de>
2017-09-24 20:30:17 +0200

    [TASK] Prevent gulp errors

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 var sourcemaps = require('gulp-sourcemaps');
+var plumber = require('gulp-plumber');
 
 gulp.task('css', function () {
   return gulp.src('./assets/scss/**/*.scss')
+    .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS())
